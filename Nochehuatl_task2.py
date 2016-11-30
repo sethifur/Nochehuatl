b0cb7c0dc72bfe2a28ad9a24d16be8189084b0cc
sethifur <sethjohns@mail.weber.edu>
2016-11-30 21:48:54 +0000

    changes date on task 2 to the correct format copied my dbconfig.py from inclass example i hope it works

--- Nochehuatl_task2.py
+++ Nochehuatl_task2.py
@@ -4,10 +4,23 @@ Create_report.py
 '''
 import sys
 
+def change_format(begin,end):
+    '''
+    changes the format of dates to YYYY-MM-DD HH:mm
+    Args:
+        begin is a date in YYYYMMDD
+        end is a date in YYYYMMDD
+    '''
+    try:
+        newBegin = begin[0:4] + '-' + begin[4:6] + '-' + begin[6:8] + " 00:00"
+        newEnd = end[0:4] + '-' + end[4:6] + '-' + end[6:8] + ' 23:59'
+    except(TypeError,ValueError):
+        exit(-1)
 
 
 # Main Function
 def main():
+    change_format(sys.argv[1],sys.argv[2])
     return
 
 if __name__ == '__main__':
