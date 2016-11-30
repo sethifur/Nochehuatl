5c660a2aea2c69eebb10f90b52f9d2229743bc85
sethifur <sethjohns@mail.weber.edu>
2016-11-30 04:22:51 +0000

    made the python template

---
+++ Nochehuatl_task2.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+'''
+Create_report.py
+'''
+import sys
+
+
+
+# Main Function
+def main():
+    return
+
+if __name__ == '__main__':
+    #Call Main
+    main()
+
+    exit(0)
