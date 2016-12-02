f5dcf144d1f07df926f00a91d88547e2be40394d
Jordan Newkirk <jordannewkirk23@gmail.com>
2016-12-01 20:25:12 -0700

    Working on ftp, still needs testing

--- Nochehuatl_task3.sh
+++ Nochehuatl_task3.sh
@@ -47,14 +47,27 @@ do
 done
 ./Nochehuatl_task2.py $beginDate $endDate
 echo "$?"
-#ftp -nv $host << End_of_Line
-#user  $userName $password
-#pwd
-#ls -la
-#cd /data
-#pwd
-#ls -la
-#End_of_Line
+
+USERN=$1
+PASSWD=$2
+
+#HOSTNAME OF YOUR FTP SERVER
+#CHANGE TO OWN ADDRESS FOR TESTING
+HOST='137.190.19.102'
+filename=company_trans_$(beginDate)_$(endDate).dat
+`gzip $filename`
+mv $filename.gz hw8Out.zip
+
+if [[ -n "$USERN" && -n "PASSWD" ]]
+then
+	echo "Using $USERN FTP account"
+	ftp -inv $HOST << EOF
+	user $USERN $PASSWD
+	cd .
+	put hw8Out.zip
+	bye
+EOF
+fi
 
 
 
